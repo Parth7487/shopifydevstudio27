86cf27e226c941b225ea0ff4641042bcd6a1cea8
Parth Patel <parth@Parths-MacBook-Pro.local>
2026-06-16 15:51:06 +0530

    Updates

---
+++ assets/ScrollToTop.C-2MvD2u.js
@@ -0,0 +1,1 @@
+import{b as o,r}from"./router.DKvCkL2M.js";import"./vendor.Ba4sUqnh.js";const t=()=>{const{pathname:t}=o();return r.useEffect(()=>{window.scrollTo(0,0)},[t]),null};export{t as default};
---
+++ assets/supabase.CWI81dEU.js
@@ -0,0 +1,1 @@
+String("").toLowerCase();let e=null;export{e as supabase};
